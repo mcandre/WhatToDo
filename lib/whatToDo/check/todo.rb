fa0fefe8127967270ceb1c168f9faca6de8cf7cc
phortx <benny@itws.de>
2013-11-18 08:37:01 +0100

    Many improvements for the todo check

--- lib/whatToDo/check/todo.rb
+++ lib/whatToDo/check/todo.rb
@@ -29,20 +29,28 @@ def code_sample(file, line)
   from = line - 4
   from = 0 if from < 0
 
-  to = line + 4
-
-  current_line = 0
   result = "\n\n"
 
-  File.open(file, 'r') do |f|
-    while read_line = f.gets
-      current_line += 1
+  snippet(file, from, line + 4).split("\n").each do |code_line|
+    active = (line == from)
+    spaces = " " * (8 - from.to_s.length - (active ? 3 : 0))
+    result << spaces + (active ? '>> '.bold.red : '')
+    result << from.to_s.bold.yellow + ": ".bold.yellow
 
-      if current_line >= from && current_line <= to
-        spaces = " " * (8 - current_line.to_s.length - (line == current_line ? 3 : 0))
-        result << spaces + (line == current_line ? '>> ' : '') + current_line.to_s + ": " + read_line
+    first = true
+
+    if code_line.length > 0
+      scan = code_line.scan(/^ +/)[0]
+      ident = scan ? scan.length + 12 : 12
+      code_line.scan(/.{1,80}/m).each do |l|
+        result << (first ? '' : ' ' * ident) + (active ? l.bold.red : l) + "\n"
+        first = false
       end
+    else
+      result << "\n"
     end
+
+    from += 1
   end
 
   result
@@ -51,8 +59,39 @@ end
 
 
 ##
+## Extracs a code snippet from a file and reduced the identation to the minimum
+##
+
+def snippet(file, from, to)
+  code = ""
+  current_line = 0
+
+  File.open(file, 'r') do |f|
+    while read_line = f.gets
+      current_line += 1
+      code << read_line.gsub(/\t/m, '  ') if current_line >= from && current_line <= to
+    end
+  end
+
+  code.gsub(/^#{code.scan(/^\s+/).min}/, '')
+end
+
+
+
+##
+## Reduces the identation of a given string to the minimum
+##
+
+def unindent(code)
+  code.gsub(/^#{scan(/^\s+/).min}/, "")
+end
+
+
+
+##
 ## Register the check script
 ##
+
 check do
   todos = get_lines.map do |line|
     splitted = line.split(':', 3)
