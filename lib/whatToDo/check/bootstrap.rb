ef6c3c500f41876924c4f2d26e9975edd92471e2
phortx <benny@itws.de>
2014-02-27 09:08:56 +0100

    Bootstrap Check: check also for script/setup

--- lib/whatToDo/check/bootstrap.rb
+++ lib/whatToDo/check/bootstrap.rb
@@ -1,7 +1,7 @@
 check do
-  if File.file?('script/bootstrap')
+  if File.file?('script/bootstrap') || File.file?('script/setup')
     nil
   else
-    "There is no script/bootstrap file in that project. Create one!"
+    "There is no 'script/bootstrap' or 'script/setup' file in that project. Create one!"
   end
 end
